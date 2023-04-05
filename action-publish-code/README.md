fb694736ff78ad4fb1b85abbbb1be9e3ec12a381
Daniel Loreto <279789+loreto@users.noreply.github.com>
2023-04-05 12:10:05 -0500

    Publish nixtest via a github action
    Create a github action to publish code and use it to publish nixtest to its own repo.

---
+++ action-publish-code/README.md
@@ -0,0 +1,20 @@
+# Publish Code (Github Action)
+### Publish code between repositories while preserving history
+![License: Apache 2.0](https://img.shields.io/github/license/jetpack-io/action-move-code)
+
+## What is it?
+A Github action that makes it easy to publish code from one git repository to another
+while preserving history. One of the primary use cases is to mirror code from a directory
+in a monorepo, and publish it as a standalone repository.
+
+This action was originally built by [jetpack.io](https://www.jetpack.io). We
+do most of our development in an [opensource monorepo](https://github.com/jetpack-io/opensource),
+and often publish some projects as separate repositories.
+
+In fact, this very repository is published using this Github Action.
+
+## Related Work
++ [Copybara](https://github.com/google/copybara): A tool written by Google to move
+  code between repositories. It is written in Java, and thus a bit heavyweight for
+  direct use. That said, a future version of this action could package Copybara in
+  an easier to use way.
---
+++ action-publish-code/publish-code.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Publishes a subdirectory in a given git repository as a standalone repository of it's own.
+
+set -euo pipefail
+set -o errexit
+set -o nounset
+
+if [ "$#" = 0 ]; then
+	echo "usage: $0 <org>/<repo> <dir1> [<dirN>]..." 1>&2
+	exit 1
+fi
+
+if [[ "$1" != *"/"* ]]; then
+	echo "error: first argument must be in the form <owner>/<repo>" 1>&2
+	echo "usage: $0 <org>/<repo> <dir1> [<dirN>]..." 1>&2
+	exit 1
+fi
+
+# Parse the source repo from the arguments:
+org="${1%/*}"
+origin_repo="${1##*/}"
+shift
+
+# Create a temporary directory into which to clone the repos:
+TMPDIR=$(mktemp -d)
+function cleanup() {
+	echo "Deleting '${TMPDIR}' ..." 1>&2
+	rm -rf "${TMPDIR}"
+}
+trap cleanup EXIT INT # Clean up on exit
+
+# Clone the origin repo:
+echo "Cloning '${org}/${origin_repo}' ..." 1>&2
+git clone -b main --single-branch "git@github.com:${org}/${origin_repo}.git" "${TMPDIR}/${origin_repo}"
+
+# Check that the directories we want to publish exist:
+echo -e "\n"
+cd "${TMPDIR}/${origin_repo}"
+for dir in "$@"; do
+	echo "Validating $dir"
+	# Just to be extra safe, make sure we're not trying to publish to origin repo itself
+	if [ "${dir}" = "${origin_repo}" ]; then
+		echo "Error: Cannot publish to the source repo: '${origin_repo}'" 1>&2
+		exit 1
+	fi
+
+	# Verify the directory exists:
+	if [ ! -d "${dir}" ]; then
+		echo "Error: Directory '${dir}' does NOT exist, cannot publish. Aborting." 1>&2
+		exit 1
+	fi
+done
+
+exit 0
+
+
+for repo in "$@"; do
+	echo "Publishing '${repo}' ..."
+
+	# Remove everything we don't want from the source repo:
+	# TODO: Rewrite using https://github.com/newren/git-filter-repo since filter-branch is no longer
+	# recommended by git.
+	FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch --tag-name-filter "grep '^${repo}/' | cut -f 2- -d '/'" --subdirectory-filter "${repo}" -- -- all
+
+	git clone "git@github.com:${org}/${repo}.git" "${TMPDIR}/${repo}"
+	pushd "${TMPDIR}/${repo}"
+
+	# Here is the trick. Connect your source repository as a remote using a local reference.
+	git remote add "${origin_repo}" "../${origin_repo}/"
+
+	# After that simply fetch the remote source, create a branch and merge it with the destination repository in usual way
+	git fetch "${origin_repo}"
+	git branch "${origin_repo}" "remotes/${origin_repo}/main"
+
+	git merge "${origin_repo}" --allow-unrelated-histories --no-edit --ff-only
+
+	# This is pretty much it, all your code and history were moved from one repository to another.
+	# All you need is to clean up a bit and push the changes to the server
+
+	git remote rm "${origin_repo}"
+	git branch -d "${origin_repo}"
+
+	git push origin main --follow-tags
+
+	popd # Back to origin repo
+
+	# Undo the filtering so we can re-use the source repo for another rewrite.
+	git for-each-ref --format="update %(refname:lstrip=2) %(objectname)" refs/original/ | git update-ref --stdin
+	git for-each-ref --format="delete %(refname) %(objectname)" refs/original/ | git update-ref --stdin
+	git reset --hard HEAD
+
+	echo "[DONE] Published '${repo}' ..."
+	echo -e "\n"
+done
