37d23998faa04e52ac2df7cadc3b966c551e2753
Daniel Loreto <279789+loreto@users.noreply.github.com>
2023-04-26 15:19:29 -0500

    [kai] Improve UX and hook it up to API (#16)

--- kai/cli/exec.go
+++ kai/cli/exec.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
@@ -22,12 +24,24 @@ func ExecCmd() *cobra.Command {
 }
 
 func execCmd(cmd *cobra.Command, args []string) error {
+	fmt.Printf("I'm Kai, the AI assistant for your terminal\n\n")
+	fmt.Printf("I can translate English into shell commands\n\n")
+
 	input := ""
-	prompt := &survey.Input{
-		Message: "What would you like to do?",
-		Help:    "Type a query to run",
+	q1 := &survey.Input{
+		Message: "What command would you like to run?",
+		Suggest: func(toComplete string) []string {
+			return []string{
+				"remove duplicates lines from file 'input.txt'",
+				"show me the contents of the file named 'input.txt'",
+				"create a new directory named 'dir'",
+				"delete the file named 'output.txt'",
+				"make all files in current directory read only",
+				"run ubuntu using docker and start an interactive shell",
+			}
+		},
 	}
-	err := survey.AskOne(prompt, &input)
+	err := survey.AskOne(q1, &input, survey.WithValidator(survey.Required))
 
 	if err != nil {
 		return err
@@ -38,9 +52,41 @@ func execCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	for _, result := range results {
-		fmt.Println(result)
+	selection := ""
+	q2 := &survey.Select{
+		Message: "Choose a command:",
+		Options: results,
+	}
+	survey.AskOne(q2, &selection)
+
+	shouldExecute := false
+	q3 := &survey.Confirm{
+		Message: "Execute this command?",
+		Default: true,
+	}
+	survey.AskOne(q3, &shouldExecute)
+
+	if shouldExecute {
+		fmt.Printf("> %s\n", selection)
+		shellexec(selection)
 	}
 
 	return nil
+}
+
+func shellexec(script string) {
+	cmd := exec.Command("sh", "-c", script)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	if err != nil {
+		// Check exit code:
+		if exitError, ok := err.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+			// Here you can get the exit code
+			os.Exit(exitError.ExitCode())
+		}
+		panic(err)
+	}
 }
