9fb57746d9c845a3fa181cfe4ba0e76c304cfa54
Mike Landau <mikeland86@gmail.com>
2023-08-21 22:49:45 -0400

    [envsec] Add auth commands (#105)
    ## Summary
    
    This PR adds auth commands (mostly for testing, we can decide what the
    keep later).
    
    It adds the following common libraries from
    https://github.com/jetpack-io/devbox to a new `/pkg` directory:
    
    * auth
    * cuecfg
    * envir (partially)
    * usererr
    * xdg
    
    Most of these were moved as-is or with minimal changes. I moved them in
    order to get `auth` working as quickly as possible, but we might not
    need them long term. If we do want to keep some/all of these we can make
    devbox point to these instead.
    
    ## How was it tested?
    
    ```bash
    envsec auth login
    envsec auth whoami
    ```

---
+++ pkg/cuecfg/toml.go
@@ -0,0 +1,16 @@
+// Copyright 2023 Jetpack Technologies Inc and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
+
+package cuecfg
+
+import (
+	"github.com/pelletier/go-toml/v2"
+)
+
+func marshalToml(v interface{}) ([]byte, error) {
+	return toml.Marshal(v)
+}
+
+func unmarshalToml(data []byte, v interface{}) error {
+	return toml.Unmarshal(data, v)
+}
---
+++ pkg/cuecfg/yaml.go
@@ -0,0 +1,17 @@
+// Copyright 2023 Jetpack Technologies Inc and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
+
+package cuecfg
+
+import "gopkg.in/yaml.v3"
+
+// TODO: consider using cue's YAML marshaller.
+// It might have extra functionality related
+// to the cue language.
+func marshalYaml(v interface{}) ([]byte, error) {
+	return yaml.Marshal(v)
+}
+
+func unmarshalYaml(data []byte, v interface{}) error {
+	return yaml.Unmarshal(data, v)
+}
