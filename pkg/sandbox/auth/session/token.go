ec13a10d74228af96e8462a819f62b04352ffdc2
Daniel Loreto <279789+loreto@users.noreply.github.com>
2023-09-15 15:47:34 -0500

    Fix parens

--- pkg/sandbox/auth/session/token.go
+++ pkg/sandbox/auth/session/token.go
@@ -11,8 +11,8 @@ type Token struct {
 	Issuer string            `json:"issuer,omitempty"`
 	Keys   []jose.JSONWebKey `json:"keys,omitempty"`
 
-	// The id token is technically contained in the original oauth2.Token (inside)
-	// of extras. However, the extras don't get serialized to JSON, so the id token
+	// The id token is technically contained in the original oauth2.Token (inside
+	// of extras). However, the extras don't get serialized to JSON, so the id token
 	// is lost of we save the token to disk and read it back. We copy the IDToken
 	// into this field so that it does get serialized properly.
 	IDToken string `json:"id_token,omitempty"`
