9193aeabee9726dbdb2a131378e386af4cdad9b4
Daniel Loreto <279789+loreto@users.noreply.github.com>
2023-09-15 15:55:15 -0500

    Add comment clarifying need for IDToken (#143)

--- pkg/sandbox/auth/session/token.go
+++ pkg/sandbox/auth/session/token.go
@@ -6,11 +6,15 @@ import (
 )
 
 type Token struct {
-	oauth2.Token // Embed and oauth2 token
+	oauth2.Token // Embed an oauth2 token
 
 	Issuer string            `json:"issuer,omitempty"`
 	Keys   []jose.JSONWebKey `json:"keys,omitempty"`
 
+	// The id token is technically contained in the original oauth2.Token (inside
+	// of extras). However, the extras don't get serialized to JSON, so the id token
+	// is lost if we save the token to disk and read it back. We copy the IDToken
+	// into this field so that it does get serialized properly.
 	IDToken string `json:"id_token,omitempty"`
 
 	idClaims *IDClaims
