4b2f0c833564f859626d947001643e1e65a95320
Mike Landau <mikeland86@gmail.com>
2023-09-20 10:57:49 -0700

    [runx] Install should return paths (#150)
    ## Summary
    
    Stacked on https://github.com/jetpack-io/opensource/pull/149
    
    `runx.Install` should return the path where binaries are installed.
    
    In the future I think we can make this API a bit better by making it
    typed. e.g.
    
    ```golang
    runx.install(git.Repo{
      Provider: git.GithubProvider,
      User:     "jetpack-io",
      Name:     "envsec",
    })
    ```
    
    ## How was it tested?
    
    runx --install jetpack-io/envsec

--- pkg/sandbox/runx/cmd/runx/cli/root.go
+++ pkg/sandbox/runx/cmd/runx/cli/root.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 
@@ -13,7 +14,10 @@ func Help() {
 	c := color.New(color.FgCyan).Add(color.Underline)
 	c.Println("runx")
 	fmt.Println()
-	fmt.Println("Usage: runx [+<org>/<repo>]... [<cmd>] [<args>]...")
+	fmt.Println(
+		"Usage: runx [+<org>/<repo>]... [<cmd>] [<args>]...",
+		"Usage: runx --install [<org>/<repo>]...",
+	)
 }
 
 func Execute(ctx context.Context, args []string) int {
@@ -22,11 +26,26 @@ func Execute(ctx context.Context, args []string) int {
 		return 0
 	}
 
-	err := runx.Run(args...)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[ERROR] %s\n", err)
-		return 1
+	install := flag.Bool("install", false, "install packages only")
+	flag.Parse()
+
+	if *install {
+		paths, err := runx.Install(args[1:]...)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[ERROR] %s\n", err)
+			return 1
+		}
+		fmt.Println("Installed paths:")
+		for _, path := range paths {
+			fmt.Printf("  %s\n", path)
+		}
+	} else {
+		if err := runx.Run(args...); err != nil {
+			fmt.Fprintf(os.Stderr, "[ERROR] %s\n", err)
+			return 1
+		}
 	}
+
 	return 0
 }
 
