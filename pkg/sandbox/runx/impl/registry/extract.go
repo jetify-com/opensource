090d91050cc99d16a133561237aac7e69b42f282
Mike Landau <mikeland86@gmail.com>
2023-10-10 11:55:27 -0700

    [runx] Allow non-archived binaries (#172)
    ## Summary
    
    Some repos (e.g. https://github.com/mvdan/gofumpt) release files as
    binaries instead of archives. This expands runx to treat unknown files
    (that still match the platform and architecture name structure) as
    binaries. It only does so if it finds a compatible artifact but the type
    is unknown.
    
    We could additionally test the mime type of the file, but it doesn't
    seem like binaries use consistent mime types.
    
    ## How was it tested?
    
    `./dist/runx +mvdan/gofumpt gofumpt --help`

--- pkg/sandbox/runx/impl/registry/extract.go
+++ pkg/sandbox/runx/impl/registry/extract.go
@@ -2,8 +2,10 @@ package registry
 
 import (
 	"context"
+	"errors"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/codeclysm/extract"
 	"go.jetpack.io/pkg/sandbox/runx/impl/fileutil"
@@ -52,4 +54,25 @@ func contentDir(path string) string {
 		return path
 	}
 	return filepath.Join(path, contents[0].Name())
+}
+
+func createSymbolicLink(src, dst, repoName string) error {
+	if err := os.MkdirAll(dst, 0700); err != nil {
+		return err
+	}
+	if err := os.Chmod(src, 0755); err != nil {
+		return err
+	}
+	binaryName := filepath.Base(src)
+	// This is a good guess for the binary name. In the future we could allow
+	// user to customize.
+	if strings.Contains(binaryName, repoName) {
+		binaryName = repoName
+	}
+	err := os.Symlink(src, filepath.Join(dst, binaryName))
+	if errors.Is(err, os.ErrExist) {
+		// TODO: verify symlink points to the right place
+		return nil
+	}
+	return err
 }
