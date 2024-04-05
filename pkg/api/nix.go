beeb869bd464979bf2dbc0fcedb5c1f733f99282
Greg Curtis <greg.curtis@jetpack.io>
2024-04-05 17:34:47 -0400

    pkg/api: add Client.GetAWSCredentials

--- pkg/api/nix.go
+++ pkg/api/nix.go
@@ -7,6 +7,14 @@ import (
 	nixv1alpha1 "go.jetpack.io/pkg/api/gen/priv/nix/v1alpha1"
 )
 
+func (c *Client) GetAWSCredentials(ctx context.Context) (*nixv1alpha1.AWSCredentials, error) {
+	r, err := c.nixClient().GetAWSCredentials(ctx, connect.NewRequest(&nixv1alpha1.GetAWSCredentialsRequest{}))
+	if err != nil {
+		return nil, err
+	}
+	return r.Msg.GetCredentials(), nil
+}
+
 func (c *Client) GetBinCache(
 	ctx context.Context,
 ) (*nixv1alpha1.GetBinCacheResponse, error) {
