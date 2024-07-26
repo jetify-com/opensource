f55f8e3fb485292891a72d7bf418aed1d6e2719a
Lucille Hua <lucille.hua@gmail.com>
2024-07-26 13:26:04 -0400

    add tsup config for api ts

---
+++ api/ts/tsup.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true, // Clean output directory before bundling
+  dts: true, // Generate .d.ts file
+  entry: ['src/**/*.ts', 'src/**/*.tsx'],
+  external: ['react', 'react-dom'],
+  format: ['esm', 'cjs'],
+  sourcemap: true, // Generate sourcemaps
+  splitting: true, // Enable code splitting into chunks
+  treeshake: true, // Remove dead code
+});
