e0e9261d68b95659ebb0da8238b0d7d5c2c58eec
Lucille Hua <lucille.hua@gmail.com>
2024-07-26 16:10:18 -0400

    bump version

--- api/ts/tsup.config.js
+++ api/ts/tsup.config.js
@@ -4,7 +4,6 @@ export default defineConfig({
   clean: true, // Clean output directory before bundling
   dts: true, // Generate .d.ts file
   entry: ['src/**/*.ts', 'src/**/*.tsx'],
-  external: ['react', 'react-dom'],
   format: ['esm', 'cjs'],
   sourcemap: true, // Generate sourcemaps
   splitting: true, // Enable code splitting into chunks
